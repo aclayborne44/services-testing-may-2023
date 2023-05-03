7f790f20c5cb2166a68a6f2a7ab733f41c0379fa
aclayborne44 <129085400+aclayborne44@users.noreply.github.com>
2023-05-03 16:46:11 -0400

    Add files via upload
    end of class

---
+++ ProductsSolution/ProductsApi/Products/ICheckForUniqueValues.cs
@@ -0,0 +1,2 @@
+﻿namespace ProductsApi.Products;
+
---
+++ ProductsSolution/ProductsApi/Products/ProductSlugUniquenessChecker.cs
@@ -0,0 +1,22 @@
+﻿using Marten;
+using SlugGenerators;
+
+namespace ProductsApi.Products;
+
+public class ProductSlugUniquenessChecker : ICheckForUniqueValues
+{
+    private readonly IDocumentSession _session;
+
+    public ProductSlugUniquenessChecker(IDocumentSession session)
+    {
+        _session = session;
+    }
+
+    public async Task<bool> IsUniqueAsync(string attempt)
+    {
+        var isFound = await _session.Query<CreateProductResponse>()
+            .Where(p => p.Slug == attempt).AnyAsync();
+
+        return !isFound;
+    }
+}
